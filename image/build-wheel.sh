f3bcfb78381eaa4f60e5e4ad38f6f72d12a65390
Betsy McPhail <betsy.mcphail@kitware.com>
2021-08-05 15:32:21 -0400

    Add initial pass at generating wheel
    Rename script to build Drake proper. Replace old script with one that
    builds only the wheel from an existing Drake install. Add an initial
    version of a setup.py to generate a wheel.
    
    This doesn't yet pass auditwheel (and may not be usable in any fashion).
    Also, the refactoring, which is to allow the Drake build to be cached by
    Docker separate from the commands specific to generating the wheel, may
    or may not "stick", as most of this ought to move to Drake's bazel
    build, which may eliminate these two steps as separate steps.
    
    Co-authored-by: Matthew Woehlke <matthew.woehlke@kitware.com>

---
+++ image/build-wheel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+# TODO most of this should move to bazel
+mkdir -p wheel/pydrake/lib
+cd /wheel
+
+cp -r -t /wheel/pydrake \
+    /opt/drake/share/doc \
+    /opt/drake/share/drake/.drake-find_resource-sentinel \
+    /opt/drake/share/drake/examples \
+    /opt/drake/share/drake/manipulation \
+    /opt/drake/lib/python*/site-packages/pydrake/*
+
+cp -r -t /wheel/pydrake/lib \
+    /opt/drake/lib/libdrake*.so
+
+cp /image/setup.py /wheel/
+
+export LD_LIBRARY_PATH=/wheel/pydrake/lib
+
+python setup.py bdist_wheel
+
+# FIXME
+# auditwheel repair dist/pydrake*.whl
